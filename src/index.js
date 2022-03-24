2edac24368bf6fc2e97c2ebe18cf469b8628c9dd
Robert <osipovitchr@gmail.com>
2022-03-24 16:22:47 -0400

    card component + nesting w/ props.children

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from '@faker-js/faker';
 import CommentDetail from './CommentDetail.js'
+import ApprovalCard from './ApprovalCard.js'
 
 
 
@@ -12,9 +13,17 @@ const App = () => {
 
     return (
         <div className="ui container comments">
-            <CommentDetail name="Robert O" date="jan 10" text="Im some text" />
-            <CommentDetail name="Dis odda Guy" date="march 4" text="me too!" />
-            <CommentDetail name="one more dude" date="feb 2" text="Dont forget about me!" />
+            <ApprovalCard>
+                <CommentDetail name="Robert O" date="jan 10" text="Im some text" avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail name="Dis odda Guy" date="march 4" text="me too!" avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                 <CommentDetail name="one more dude" date="feb 2" text="Dont forget about me!" avatar={faker.image.avatar()} />
+            </ApprovalCard>
+           
+                
             
             
         </div>
