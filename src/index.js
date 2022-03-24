da646534235d9b890bc3413be450458645130959
Robert <osipovitchr@gmail.com>
2022-03-24 13:08:45 -0400

    integrate semantic UI + delete curr JSX

--- src/index.js
+++ src/index.js
@@ -5,17 +5,11 @@ import ReactDOM from "react-dom";
 
 
 const App = () => {
-const buttonText = {text: 'Click Me'}
+
 
 
     return (
-        <div>
-        <label className='label' htmlFor='name'>
-            Enter Name:
-        </label>
-        <input id='name' type='text' />
-            <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText.text}</button>
-        </div>
+        <h1>Hi There</h1>
     )
 }
 
