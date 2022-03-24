863e7e04bb0fa2bbbddf2b7a1de437b20392c536
Robert <osipovitchr@gmail.com>
2022-03-24 12:40:04 -0400

    play around w/ JSX + udemy challenge

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
+
+
 const App = () => {
-    return <h1>Hello</h1>
+const buttonText = {text: 'Click Me'}
+
+
+    return (
+        <div>
+        <label className='label' htmlFor='name'>
+            Enter Name:
+        </label>
+        <input id='name' type='text' />
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText.text}</button>
+        </div>
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
